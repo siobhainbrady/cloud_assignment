8c25279da2ca1c6e4fc02262a3c10bf8ae751e80
siobhainbrady <siobhain.brady94@gmail.com>
2017-04-14 12:52:02 +0100

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+
+define('DB_HOST', '35.184.146.33');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'shopdatabase');
+define('DB_NAME', 'shop_database');
+
+
+//Establish connection with the server
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+ ?>
